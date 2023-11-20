b1665707ad0ffad02a96d3f0b2d26959d5ff5965
MarionDuFaouet <lozachm@gmail.com>
2023-11-20 13:02:55 +0100

    ajout fichier js

---
+++ eleve.js
@@ -0,0 +1,8 @@
+class Eleve {
+    constructor(nom, prenom, classe) {
+        this.name=nom;
+        this. firstname=prenom;
+        this.course=classe;
+        this.notes={maths:[], algorithmique:[], javascript:[]};
+    }
+}
