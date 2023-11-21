ba10ae1653264a921801b38f8b8c08dcaad3e52c
MarionDuFaouet <lozachm@gmail.com>
2023-11-21 19:47:14 +0100

    correction

--- js/eleve.js
+++ js/eleve.js
@@ -1,7 +1,7 @@
 class Eleve {
     constructor(nom, prenom, classe) {
         this.name = nom;
-        this.firstname = prenom;
+        this.firstName = prenom;
         this.course = classe;
         this.notes = {
             maths: [],
@@ -14,20 +14,11 @@ class Eleve {
 
     //envoi une alert comprenant trois paramètres
     bonjour() {
-        alert(`Bonjour, je suis ${this.firstname} ${this.name} en classe ${this.course} !`);
+        alert(`Bonjour, je suis ${this.firstName} ${this.name} en classe ${this.course} !`);
     }
-
-    /* 
-     * Ajoute une note pour une matière donnée
-     * Parameters:
-     * - matiere: matière impactée
-     * - note: note ajoutée pour la matière concernée
-     * Retours -
-     */
 
     //ajoute des notes au tableau
     addNoteAlgo(n) {
         this.notes.algorithmique.push(n);
     }
 }
-//test
